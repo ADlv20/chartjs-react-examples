2192f55bcc64f90c90083cec831be3fce5dca5f5
ADlv19 <adwaitg02@gmail.com>
2022-07-17 10:59:11 +0530

    init

--- src/Components/BrChart.jsx
+++ src/Components/BrChart.jsx
@@ -17,12 +17,12 @@ export const dataBar = {
   labels,
   datasets: [
     {
-      label: "IDK 1",
+      label: "Rescued",
       data: [20, 30, 40, 50, 60, 70, 80],
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
-      label: "IDK 2",
+      label: "Affected",
       data: [40, 50, 60, 70, 80, 90, 100],
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
